4322eee10e2f5323d3128a7569af9a4949f9ec3c
Byeoungwook Kim <quddnr145@gmail.com>
2016-02-19 19:18:06 +0900

    fix display error to attached usb list
    1) usb 목록을 출력시키기 위한 명령어 버그 수정
    2) rtl8188cu chipset 기준의 WLAN adapter QEMU 시뮬레이션 가능하도록 수정
    3) webcam(odroid HD 720p) usb 제품을 QEMU 시뮬레이션 가능하도록 수정
    
    Signed-off-by: Byeoungwook Kim <quddnr145@gmail.com>

--- run-diskmode.sh
+++ run-diskmode.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 if test -e "/home/`whoami`/brdisk-img.raw"; then
-  qemu-system-i386 -m 512 -hda /home/`whoami`/brdisk-img.raw
+  device_name="Realtek"
+  realtek_wifi_usb=`lsusb | grep $device_name | awk '{split($$0,arr," "); printf arr[6];}'`
+  device_name="Fushicai"
+  webcam_usb="`lsusb | grep $device_name | awk '{split($$0,arr," "); printf arr[6];}'`"
+  webcam_vendor_id=`echo $webcam_usb | awk '{split($$0,arr,":"); printf arr[1];}'`
+  webcam_product_id=`echo $webcam_usb | awk '{split($$0,arr,":"); printf arr[2];}'`
+
+  sudo qemu-system-i386 \
+	  -enable-kvm \
+	  -m 512 \
+	  -hda /home/`whoami`/brdisk-img.raw \
+	  -device usb-ehci,id=ehci \
+	  -device usb-host,vendorid=0x$webcam_vendor_id,productid=0x$webcam_product_id,bus=ehci.0,port=1 \
+	  -usbdevice host:$realtek_wifi_usb \
+	  -serial stdio
 else
   echo "Not found brdisk-img.raw"
   echo "Execute compile-diskmode.sh"
