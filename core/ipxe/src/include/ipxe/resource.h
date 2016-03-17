70d969250f7133aff05abab1ffd522c3afaf056d
Byeoungwook Kim <quddnr145@gmail.com>
2016-03-18 05:11:30 +0900

    add png image file for project logo
    logo.png : img_logo[], img_logo_len
    
    Signed-off-by: Byeoungwook Kim <quddnr145@gmail.com>

--- core/ipxe/src/include/ipxe/resource.h
+++ core/ipxe/src/include/ipxe/resource.h
@@ -1,10 +1,8 @@
 #ifndef _IPXE_RESOURCE_H
 #define _IPXE_RESOURCE_H
 
-extern unsigned char img_d2logo[];
-extern unsigned char img_title[];
-extern int img_d2logo_len;
-extern int img_title_len;
+extern unsigned char img_logo[];
+extern int img_logo_len;
 
 #endif
 
