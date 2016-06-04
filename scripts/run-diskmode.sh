5d64870c460c17353ea43e2175a0c1b1095079dd
ByeoungWook Kim <quddnr145@gmail.com>
2016-06-04 16:16:44 +0900

    fix makefile about ipxe & grub
    supported 'make MODE=[disk|net] run'

--- scripts/run-diskmode.sh
+++ scripts/run-diskmode.sh
@@ -2,19 +2,19 @@
 if test -e "/home/`whoami`/brdisk-img.raw"; then
   device_name="Realtek"
   realtek_wifi_usb=`lsusb | grep $device_name | awk '{split($$0,arr," "); printf arr[6];}'`
-  device_name="Fushicai"
+  device_name="VirtualBox"
   webcam_usb="`lsusb | grep $device_name | awk '{split($$0,arr," "); printf arr[6];}'`"
   webcam_vendor_id=`echo $webcam_usb | awk '{split($$0,arr,":"); printf arr[1];}'`
   webcam_product_id=`echo $webcam_usb | awk '{split($$0,arr,":"); printf arr[2];}'`
 
   sudo qemu-system-i386 \
-	  -enable-kvm \
 	  -m 512 \
 	  -hda /home/`whoami`/brdisk-img.raw \
 	  -device usb-ehci,id=ehci \
-	  -device usb-host,vendorid=0x$webcam_vendor_id,productid=0x$webcam_product_id,bus=ehci.0,port=1 \
-	  -usbdevice host:$realtek_wifi_usb \
+	  -usbdevice host:80ee:0030 \
 	  -serial stdio
+#	  -usbdevice host:$realtek_wifi_usb \
+#	  -device usb-host,vendorid=0x$webcam_vendor_id,productid=0x$webcam_product_id,bus=ehci.0,port=1 \
 else
   echo "Not found brdisk-img.raw"
   echo "Execute compile-diskmode.sh"
