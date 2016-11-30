1b6e0bfd8dde355a4f9170a66bee9ee3760d132d
Byeoungwook Kim <quddnr145@gmail.com>
2016-11-30 22:56:04 +0900

    build 상태시 오류 발생 부분을 체크하기 위한 디버깅 문자열 추가

--- build.sh
+++ build.sh
@@ -16,18 +16,18 @@ for x in $compile_list; do
         # TODO : 이곳에 *.pro 파일을 해석한 내용을 입력한다
 
         echo "qmake $pro_file_path"
-        qmake $pro_file_path
-        make
+        `qmake $pro_file_path`
+        `make`
 
         echo "./$filename"
         `./$filename`
 
         #for gcovname in `find $tests_path/. | egrep '\.cpp'`; do
-        #  gcov-5 -n -o $tests_path/. $gcovname > /dev/null;
+        #  `gcov-5 -n -o $tests_path/. $gcovname > /dev/null`;
         #done
 
         for gcovname in $SOURCES; do
-          gcov-5 -n -o $tests_path/. $gcovname > /dev/null;
+          `gcov-5 -n -o $tests_path/. $gcovname > /dev/null`;
         done
       else
         # TODO : 이곳에 *.pro 파일을 발견하지 못했다는 오류를 출력한다.
