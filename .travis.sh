1346cfc3c3b358a0d5e9892b0c225708a1c087f2
Byeoungwook Kim <quddnr145@gmail.com>
2016-11-30 21:47:31 +0900

    codecov 적용을 위한 travis-ci yml 파일 수정

--- .travis.sh
+++ .travis.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
 tests_path="`pwd`/tests"
 # get all test cpp files
 compile_list=`ls -l $tests_path | grep ^- | awk '{split($0,arr," "); print arr[9];}'`
@@ -24,21 +23,47 @@ for x in $compile_list; do
           SOURCES[$i]=$tests_path"/"${SOURCES[$i]}
         done
 
+        SOURCES += ${SOURCES[@]} $tests_path/test_shell.cpp $tests_path/$x
+
+        QMAKE_CXXFLAGS += -Wall -Wextra -Weffc++ -Werror
+
+        # gcov
+        QMAKE_CXXFLAGS += -fprofile-arcs -ftest-coverage
+        LIBS += -lgcov -lpthread
+
+        # C++11
+        QMAKE_CXX = g++-5
+        QMAKE_LINK = g++-5
+        QMAKE_CC = gcc-5
+        QMAKE_CXXFLAGS += -std=c++11
+
+        `qmake travis_qmake_gcc_cpp11_gcov.pro`
+        `make`
+
+        `./travis_qmake_gcc_cpp11_gcov`
+
+        for filename in `find $tests_path/. | egrep '\.cpp'`; do
+          gcov-5 -n -o $tests_path/. $filename > /dev/null;
+        done
+        for filename in ${!SOURCES[*]}; do
+          gcov-5 -n -o $tests_path/. $filename > /dev/null;
+        done
+
         # build and execute
-        build_command="g++ -coverage -o $tests_path/tests ${HEADERS[@]} ${SOURCES[@]}"
-        build_command="$build_command $tests_path/test_shell.cpp $tests_path/$x -lpthread"
-        exec_command="$tests_path/tests"
+        #build_command="g++ -o $tests_path/tests ${HEADERS[@]} ${SOURCES[@]}"
+        #build_command="$build_command $tests_path/test_shell.cpp $tests_path/$x -lpthread"
+        #exec_command="$tests_path/tests"
 
-        echo $build_command
-        echo $exec_command
-        `$build_command`
-        `$exec_command &> /dev/null`
+        #echo $build_command
+        #echo $exec_command
+        #`$build_command`
+        #`$exec_command &> /dev/null`
 
-        result=$?
-        if [ "$result" != "0" ]; then
-          echo "$x($result): exit $result"
-          exit $result
-        fi
+        #result=$?
+        #if [ "$result" != "0" ]; then
+        #  echo "$x($result): exit $result"
+        #  exit $result
+        #fi
       else
         # TODO : 이곳에 *.dep 파일을 발견하지 못했다는 오류를 출력한다.
         echo "Not found file about $dep_file_path"
@@ -46,4 +71,5 @@ for x in $compile_list; do
     fi
   fi
 done
+
 
